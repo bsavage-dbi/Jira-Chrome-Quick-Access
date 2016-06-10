9af11c7abb7ed14f9b23c7d193938ffa43b2d136
Ervin Ebalo <eebalo@gmail.com>
2016-06-10 15:57:14 -0700

    initial commit

---
+++ jira.js
@@ -0,0 +1,39 @@
+$( document ).ready(function() {
+    $("#jiraKey").focus();
+});
+$( "#showOptions" ).click(function() {
+  if (chrome.runtime.openOptionsPage) {
+    // New way to open options pages, if supported (Chrome 42+).
+    chrome.runtime.openOptionsPage();
+  } else {
+    // Reasonable fallback.
+    window.open(chrome.runtime.getURL('options.html'));
+  }
+});
+
+
+$( "#jiraKeyForm" ).submit(function( event ) {
+  chrome.storage.sync.get("jiraServer", function (obj) {
+    window.open(obj.jiraServer + "/browse/" + $( '#jiraKey' ).val());
+  });
+  
+  event.preventDefault();
+});
+
+$( "#jiraSearchForm" ).submit(function( event ) {
+  chrome.storage.sync.get("jiraServer", function (obj) {
+    window.open(obj.jiraServer + "/issues/?jql=" + $( '#jql' ).val());
+  });
+  event.preventDefault();
+});
+
+
+$( "#createIssue" ).click(function( event ) {
+  console.log('hi');
+  chrome.storage.sync.get("jiraServer", function (obj) {
+    window.open(obj.jiraServer + "/secure/CreateIssue!default.jspa");
+  });
+  
+  event.preventDefault();
+});
+
